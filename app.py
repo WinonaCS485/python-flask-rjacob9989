f4924c2d352f5c6db7ea4d44c4683787cc487831
rjacob9989 <rhettjjacobs@gmail.com>
2020-02-04 12:53:08 -0600

    Adding Dynamic test.
    Adding Dynamic test.

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def cakes():
     return 'Yummy cakes!'
 
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=6352)

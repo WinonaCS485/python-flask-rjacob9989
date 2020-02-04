10653de2f18d6aa6a5e9dc06300751f7c18f7183
rjacob9989 <rhettjjacobs@gmail.com>
2020-02-04 14:03:24 -0600

    add databse to flask
    add databse to flask

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
 from flask import Flask, render_template
+import pymysql.cursors
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+
+    # Connect to the database
+    connection = pymysql.connect(host='mrbartucz.com',
+                                user='av6352tk',
+                                password='Buddha414!',
+                                db='av6352tk_University',
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor)
+    try:
+        with connection.cursor() as cursor:
+         # Select all Students
+         key = input("Enter a name to search:\n" )
+         sql = "SELECT * from Students WHERE Name LIKE %s"
+        
+         # execute the SQL command
+         cursor.execute(sql, (key,))
+        
+        string_result =  cursor
+
+         # get the results
+        for result in cursor:
+            print (result)
+
+      
+        # If you INSERT, UPDATE or CREATE, the connection is not autocommit by default.
+        # So you must commit to save your changes. 
+        # connection.commit()
+        
+
+    finally:
+        connection.close()
+
+        return render_template('db_test.html', result =result )
+
     return render_template('index.html')
 
 @app.route('/cakes')
@@ -13,6 +48,7 @@ def cakes():
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('page.html', name=name)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=6352)
